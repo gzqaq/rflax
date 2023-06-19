acdc10654105ab1c203512ef2a98332972e40a3a
Ziqin Gong <gongzq0301@sjtu.edu.cn>
2023-06-19 21:17:00 +0800

    Correct calculation of q_learning_loss

--- rflax/components/loss.py
+++ rflax/components/loss.py
@@ -9,6 +9,6 @@ import jax.numpy as jnp
 @jax.jit
 def q_learning_loss(q_vals: Array, target_q_vals: Array, rewards: Array,
                     discount: float, masks: Array) -> Array:
-  td_target = rewards + discount * (1 - masks) * target_q_vals
+  td_target = rewards + discount * masks * target_q_vals
 
   return jnp.mean(jnp.power(q_vals - td_target, 2))
