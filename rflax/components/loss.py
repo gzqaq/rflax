b69976bf2a6d9a8c828ef35707d2e129234b7f0c
Ziqin Gong <gongzq0301@sjtu.edu.cn>
2023-07-04 15:53:10 +0800

    Reshape rewards and masks when computing q_learning_loss

--- rflax/components/loss.py
+++ rflax/components/loss.py
@@ -9,6 +9,8 @@ import jax.numpy as jnp
 @jax.jit
 def q_learning_loss(q_vals: Array, target_q_vals: Array, rewards: Array,
                     discount: float, masks: Array) -> Array:
+  rewards = jnp.reshape(rewards, q_vals.shape)
+  masks = jnp.reshape(masks, q_vals.shape)
   td_target = rewards + discount * masks * target_q_vals
 
   return jnp.mean(jnp.power(q_vals - td_target, 2))
