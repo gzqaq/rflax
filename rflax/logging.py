952d98f4328e4c4a2fd8ac26f82c9d9dbf72ac90
Ziqin Gong <gongzq0301@sjtu.edu.cn>
2023-07-15 20:23:52 +0800

    Add wandb logger

--- rflax/logging.py
+++ rflax/logging.py
@@ -1,7 +1,14 @@
+import os
 import pprint
+import tempfile
+import uuid
+import wandb
 from absl import flags, logging
+from copy import copy
 from ml_collections import ConfigDict
 from ml_collections.config_flags import config_flags
+from ml_collections.config_dict import config_dict
+from socket import gethostname
 
 
 def define_flags_with_default(**kwargs):
@@ -51,6 +58,8 @@ def flatten_config_dict(config, prefix=None):
       next_prefix = key
     if isinstance(val, ConfigDict):
       output.update(flatten_config_dict(val, prefix=next_prefix))
+    elif isinstance(val, dict):
+      output.update(flatten_config_dict(ConfigDict(val), prefix=next_prefix))
     else:
       output[next_prefix] = val
   return output
@@ -58,3 +67,71 @@ def flatten_config_dict(config, prefix=None):
 
 def prefix_metrics(metrics, prefix):
   return {"{}/{}".format(prefix, key): value for key, value in metrics.items()}
+
+
+class WandBLogger(object):
+  @staticmethod
+  def get_default_config(updates=None):
+    config = ConfigDict()
+    config.online = False
+    config.prefix = "rflax"
+    config.project = "jax"
+    config.output_dir = "/tmp/rflax"
+    config.name = config_dict.placeholder(str)
+    config.anonymous = config_dict.placeholder(str)
+    config.notes = config_dict.placeholder(str)
+
+    if updates is not None:
+      config.update(ConfigDict(updates).copy_and_resolve_references())
+    return config
+
+  def __init__(self, config, variant):
+    self.config = self.get_default_config(config)
+
+    if self.config.name is None:
+      self.config.name = uuid.uuid4().hex[:8]
+
+    if self.config.prefix != "":
+      self.config.project = f"{self.config.prefix}-{self.config.project}"
+
+    if self.config.output_dir == "":
+      self.config.output_dir = tempfile.mkdtemp()
+    else:
+      self.config.output_dir = os.path.join(self.config.output_dir,
+                                            self.config.name)
+      os.makedirs(self.config.output_dir, exist_ok=True)
+
+    self._variant = copy(variant)
+
+    if "hostname" not in self._variant:
+      self._variant["hostname"] = gethostname()
+
+    self.run = wandb.init(
+        reinit=True,
+        config=self._variant,
+        project=self.config.project,
+        dir=self.config.output_dir,
+        name=self.config.name,
+        anonymous=self.config.anonymous,
+        notes=self.config.notes,
+        settings=wandb.Settings(
+            start_method="thread",
+            _disable_stats=True,
+        ),
+        mode="online" if self.config.online else "offline",
+    )
+
+  def log(self, *args, **kwargs):
+    self.run.log(*args, **kwargs)
+
+  @property
+  def run_name(self):
+    return self.config.name
+
+  @property
+  def variant(self):
+    return self._variant
+
+  @property
+  def output_dir(self):
+    return self.config.output_dir
