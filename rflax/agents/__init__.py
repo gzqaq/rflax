91f229e8de0334e0d364a83b048e65eee09474eb
Ziqin Gong <gongzq0301@sjtu.edu.cn>
2023-10-07 21:28:41 +0800

    Build with poetry

--- rflax/agents/__init__.py
+++ rflax/agents/__init__.py
@@ -1,70 +1,70 @@
 """Implementations of RL agents."""
 
-from rflax.types import Array, ConfigDictLike, VariableDict
+# from rflax.types import Array, ConfigDictLike, VariableDict
 
-import chex
-import jax
-from copy import deepcopy
-from ml_collections import ConfigDict
-from typing import Optional, Sequence
+# import chex
+# import jax
+# from copy import deepcopy
+# from ml_collections import ConfigDict
+# from typing import Optional, Sequence
 
-@chex.dataclass(frozen=True)
-class AgentConfig:
-  pass
-
-
-class Agent(object):
-  @staticmethod
-  def default_config():
-    raise NotImplementedError
-
-  def __init__(self, config, obs_dim: int) -> None:
-    self.config = config
-    self._obs_dim = obs_dim
-    self._step = 0
-
-  @property
-  def obs_dim(self) -> int:
-    return self._obs_dim
-
-  @property
-  def step(self) -> int:
-    return self._step
+# @chex.dataclass(frozen=True)
+# class AgentConfig:
+#   pass
 
 
-class DiscreteAgent(Agent):
-  def __init__(self, config, obs_dim: int, n_actions: int) -> None:
-    super().__init__(config, obs_dim)
+# class Agent(object):
+#   @staticmethod
+#   def default_config():
+#     raise NotImplementedError
 
-    self._n_actions = n_actions
+#   def __init__(self, config, obs_dim: int) -> None:
+#     self.config = config
+#     self._obs_dim = obs_dim
+#     self._step = 0
 
-  @property
-  def n_actions(self) -> int:
-    return self._n_actions
+#   @property
+#   def obs_dim(self) -> int:
+#     return self._obs_dim
 
-
-class ContinuousAgent(Agent):
-  def __init__(self, config, obs_dim: int,
-               action_bound: Sequence[Array]) -> None:
-    super().__init__(config, obs_dim)
-
-    self.action_high = jax.device_put(action_bound[0])
-    self.action_low = (-action_bound[0] if len(action_bound) == 1 else
-                       jax.device_put(action_bound[1]))
-    self._action_dim = self.action_high.shape[0]
-
-  @property
-  def action_dim(self) -> int:
-    return self._action_dim
+#   @property
+#   def step(self) -> int:
+#     return self._step
 
 
-class TargetParams(object):
-  """Store params of target networks."""
-  def __init__(self, **kwargs):
-    self._keys = []
-    for k, v in kwargs.items():
-      setattr(self, k, deepcopy(v))
-      self._keys.append(k)
+# class DiscreteAgent(Agent):
+#   def __init__(self, config, obs_dim: int, n_actions: int) -> None:
+#     super().__init__(config, obs_dim)
 
-  def to_dict(self) -> VariableDict:
-    return {k: getattr(self, k) for k in self._keys}
+#     self._n_actions = n_actions
+
+#   @property
+#   def n_actions(self) -> int:
+#     return self._n_actions
+
+
+# class ContinuousAgent(Agent):
+#   def __init__(self, config, obs_dim: int,
+#                action_bound: Sequence[Array]) -> None:
+#     super().__init__(config, obs_dim)
+
+#     self.action_high = jax.device_put(action_bound[0])
+#     self.action_low = (-action_bound[0] if len(action_bound) == 1 else
+#                        jax.device_put(action_bound[1]))
+#     self._action_dim = self.action_high.shape[0]
+
+#   @property
+#   def action_dim(self) -> int:
+#     return self._action_dim
+
+
+# class TargetParams(object):
+#   """Store params of target networks."""
+#   def __init__(self, **kwargs):
+#     self._keys = []
+#     for k, v in kwargs.items():
+#       setattr(self, k, deepcopy(v))
+#       self._keys.append(k)
+
+#   def to_dict(self) -> VariableDict:
+#     return {k: getattr(self, k) for k in self._keys}
--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import find_packages, setup
-
-setup(
-    name="rflax",
-    version="0.0.0",
-    author="Ziqin Gong",
-    packages=find_packages(),
-)
